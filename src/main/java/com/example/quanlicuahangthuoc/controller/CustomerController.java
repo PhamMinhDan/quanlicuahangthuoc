aeb5a31970f8fe538b61f00ed6c8c6b8e48b386a
tennguoi <nguyenduccongminh@gmail.com>
2025-10-18 20:37:47 +0700

    small update

--- src/main/java/com/example/quanlicuahangthuoc/controller/CustomerController.java
+++ src/main/java/com/example/quanlicuahangthuoc/controller/CustomerController.java
@@ -2,19 +2,14 @@ package com.example.quanlicuahangthuoc.controller;
 
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.quanlicuahangthuoc.entity.Customer;
 import com.example.quanlicuahangthuoc.service.CustomerService;
-
-import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/customers")
@@ -40,13 +35,5 @@ public class CustomerController {
             return ResponseEntity.notFound().build();
         }
     }
-    @PostMapping
-    public ResponseEntity<?> createCustomer(@Valid @RequestBody Customer customer) {
-        try {
-            Customer createdCustomer = customerService.createCustomer(customer);
-            return ResponseEntity.status(HttpStatus.CREATED).body(createdCustomer);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-        }
-    }
+    
 }
