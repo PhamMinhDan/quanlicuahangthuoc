afc3de0bba509258dab610fb0731c3723e622622
Pham Minh Dan <phamdanngu937@gmail.com>
2025-10-18 23:34:37 +0700

    pull code new

--- src/main/java/com/example/quanlicuahangthuoc/service/PromotionService.java
+++ src/main/java/com/example/quanlicuahangthuoc/service/PromotionService.java
@@ -8,8 +8,19 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import com.example.quanlicuahangthuoc.entity.Staff;
+import com.example.quanlicuahangthuoc.repository.StaffRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class PromotionService{
+   
+}
 
 
 
