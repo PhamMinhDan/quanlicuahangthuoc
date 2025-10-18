dcf8bf830267bcd6c28ca4999b3f3db1beb80bc2
tennguoi <nguyenduccongminh@gmail.com>
2025-10-18 20:38:33 +0700

    dọn rác

--- src/main/java/com/example/quanlicuahangthuoc/controller/CustomerController.java
+++ src/main/java/com/example/quanlicuahangthuoc/controller/CustomerController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,14 +25,6 @@ public class CustomerController {
         return ResponseEntity.ok(customers);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Customer> getCustomerById(@PathVariable Integer id) {
-        try {
-            Customer customer = customerService.getCustomerById(id);
-            return ResponseEntity.ok(customer);
-        } catch (Exception e) {
-            return ResponseEntity.notFound().build();
-        }
-    }
+    
     
 }
