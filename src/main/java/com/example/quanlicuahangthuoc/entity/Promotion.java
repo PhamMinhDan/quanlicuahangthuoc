9cfc4f8cd418ace2f042eba781aad7724766e7bd
thanhngan1606 <nttngan1606@gmail.com>
2025-10-23 00:57:04 +0700

    sua thoi gian

--- src/main/java/com/example/quanlicuahangthuoc/entity/Promotion.java
+++ src/main/java/com/example/quanlicuahangthuoc/entity/Promotion.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.FutureOrPresent;
 import org.springframework.format.annotation.DateTimeFormat;
 import java.time.LocalDate;
 
@@ -31,6 +32,7 @@ public class Promotion {
     private Double discountPercent;
 
     @NotNull(message = "Expired date must not be null")
+    @FutureOrPresent(message = "Expired date must be today or in the future")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     @Column(name = "validity_period", nullable = false)
     private LocalDate expiredDate;
